299198086332cd8d0489e0717a138b0db03b03b1
SarahC233 <122423221+SarahC233@users.noreply.github.com>
2024-05-28 20:44:58 +1200

    simplified server, quote calc page working with this

--- server.js
+++ server.js
@@ -1,45 +1,26 @@
-// ====== Packages and imports ====== //
 const express = require("express");
 const cors = require("cors");
-const app = express();
 require("dotenv").config();
 
- 
+const app = express(); // Initialize app first
 
 // Import routes
-const carValueCalc = require("./routes/carValueCalc.js");
-const riskRatingCalc = require("./routes/riskRatingCalc.js");
 const quoteCalc = require("./routes/quoteCalc.js");
 
 // Middleware
-app.use(cors({ origin: process.env.SITE_URL || "*" })); //using * to allow all origins, may have fixed the bug in quoteCalc
+app.use(cors({ origin: process.env.SITE_URL || "*" }));
 app.use(express.json());
 
 // =========== ENDPOINTS =========== //
-// Initial setup in Postman
 app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
 
-// Car value calculation api
-app.use("/api", carValueCalc);
-
-// Risk rating calculation api
-app.use("/api", riskRatingCalc);
-
 // Quote calculation api
-app.use("/api", quoteCalc);
+app.use(quoteCalc);
 
 // ============== PORT ============== //
-const PORT = process.env.PORT;
-app
-  .listen(PORT, () => {
-    console.log(`Server is alive on http://localhost:${PORT}`);
-  })
-  .on("error", (error) => {
-    if (error.code === "EADDRINUSE") {
-      console.log("PORT is already in use.");
-    } else {
-      console.log("Server Errors: ", error);
-    }
-  });
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is alive on http://localhost:${PORT}`);
+});
